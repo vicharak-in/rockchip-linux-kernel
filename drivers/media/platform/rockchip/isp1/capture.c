18a56056f70b248a51f7d7174604ce89e375cf0b
Shunqian Zheng <zhengsq@rock-chips.com>
2020-03-25 10:53:04 +0800

    BACKPORT: media: rockchip: isp1: set height alignment to 16 in queue_setup
    Round up height to align with 16 when allocating memory so that
    Rockchip encoder(e.g. h264encoder, jpeg encoder) can use ISP1 DMA
    buffer directly, though this may waste a bit of memory.
    
    For example, to encode 1920x1080 NV12, Rockchip encoder requires memory
    size not less than 1920 * 1088 * 3 / 2.
    
    Change-Id: Ie1b295cffa71ca29395236ef9cffffb4715be9ed
    Signed-off-by: Shunqian Zheng <zhengsq@rock-chips.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/media/platform/rockchip/isp1/capture.c
+++ drivers/media/platform/rockchip/isp1/capture.c
@@ -81,6 +81,12 @@
 #define STREAM_MIN_MP_SP_INPUT_WIDTH		32
 #define STREAM_MIN_MP_SP_INPUT_HEIGHT		32
 
+/*
+ * Round up height when allocate memory so that Rockchip encoder can
+ * use DMA buffer directly, though this may waste a bit of memory.
+ */
+#define MEMORY_ALIGN_ROUND_UP_HEIGHT		16
+
 /* Get xsubs and ysubs for fourcc formats
  *
  * @xsubs: horizontal color samples in a 4*4 matrix, for yuv
@@ -1306,9 +1312,10 @@ static int rkisp1_queue_setup(struct vb2_queue *queue,
 
 	for (i = 0; i < isp_fmt->mplanes; i++) {
 		const struct v4l2_plane_pix_format *plane_fmt;
+		int h = round_up(pixm->height, MEMORY_ALIGN_ROUND_UP_HEIGHT);
 
 		plane_fmt = &pixm->plane_fmt[i];
-		sizes[i] = plane_fmt->sizeimage;
+		sizes[i] = plane_fmt->sizeimage / pixm->height * h;
 	}
 
 	v4l2_dbg(1, rkisp1_debug, &dev->v4l2_dev, "%s count %d, size %d\n",
