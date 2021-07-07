8e6381ab75fc0a3f484615f2ec866d31440c71de
Sandy Huang <hjc@rock-chips.com>
2021-07-07 14:30:24 +0800

    drm/rockchip: don't mask possible_crtcs if remote-point is disabled
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>
    Change-Id: Id8bb14fdb025f43da5d10626b660c2fcad490e39
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpu/drm/rockchip/cdn-dp-core.c
+++ drivers/gpu/drm/rockchip/cdn-dp-core.c
@@ -1237,8 +1237,8 @@ static int cdn_dp_bind(struct device *dev, struct device *master, void *data)
 
 	encoder = &dp->encoder;
 
-	encoder->possible_crtcs = drm_of_find_possible_crtcs(drm_dev,
-							     dev->of_node);
+	encoder->possible_crtcs = rockchip_drm_of_find_possible_crtcs(drm_dev,
+								      dev->of_node);
 	DRM_DEBUG_KMS("possible_crtcs = 0x%x\n", encoder->possible_crtcs);
 
 	ret = drm_encoder_init(drm_dev, encoder, &cdn_dp_encoder_funcs,
