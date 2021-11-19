6613771ca0abcc4ff9947ec26fd3128d7dabd47b
Ding Wei <leo.ding@rock-chips.com>
2021-11-19 19:20:57 +0800

    video: rockchip: mpp: move session->wait to task->wait
    reason:
    1. If the task in the session is assigned to two different cores, the
    task in the back may complete first and trigger the wait signal of the
    session.
    2. In the wait thread, it is taken according to the pending list order,
    and the previous task will be triggered, and it must not be found in
    the list.
    3. For the above reasons, put the wait signal into the task and monitor
    the wait signal of the task according to the pending order, then the
    order will not be wrong.
    4. According to this method, done_list can also be unnecessary.
    
    relative log:
    session 000000003edbfbaa task 00000000207419c6, not found in done list!
    session 000000003edbfbaa task 000000006a0597ed, not found in done list!
    session 000000003edbfbaa task 00000000fe35cb90, not found in done list!
    session 000000003edbfbaa task 000000001638cf87, not found in done list!
    session 000000003edbfbaa task 00000000a2ba5976, not found in done list!
    
    Change-Id: I2ab6c98162cc4b71ef34cbcda0bc30434fecdb5d
    Signed-off-by: Ding Wei <leo.ding@rock-chips.com>

--- drivers/video/rockchip/mpp/mpp_common.h
+++ drivers/video/rockchip/mpp/mpp_common.h
@@ -353,13 +353,7 @@ struct mpp_session {
 	struct mutex pending_lock;
 	/* task pending list in session */
 	struct list_head pending_list;
-	/* lock for session task done list */
-	struct mutex done_lock;
-	/* task done list in session */
-	struct list_head done_list;
 
-	/* event for session wait thread */
-	wait_queue_head_t wait;
 	pid_t pid;
 	atomic_t task_count;
 	atomic_t release_request;
@@ -461,6 +455,8 @@ struct mpp_task {
 	u32 *reg;
 	/* hw cycles */
 	u32 hw_cycles;
+	/* event for session wait thread */
+	wait_queue_head_t wait;
 };
 
 struct mpp_taskqueue {
