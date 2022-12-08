9fbd8f8d5cd32b419ba4f01672fc38e252b9ba2a
Sandy Huang <hjc@rock-chips.com>
2022-12-08 11:46:29 +0800

    soc: rockchip: cpuinfo: Add support for rk3528
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>
    Change-Id: I0c2ba1875faa224afd60065f7a17b4d247ac0ab0

--- include/linux/rockchip/cpu.h
+++ include/linux/rockchip/cpu.h
@@ -24,6 +24,7 @@
 #define ROCKCHIP_CPU_RK312X		0x31260000
 #define ROCKCHIP_CPU_RK3288		0x32880000
 #define ROCKCHIP_CPU_RK3308		0x33080000
+#define ROCKCHIP_CPU_RK3528		0x35280000
 #define ROCKCHIP_CPU_RK3566		0x35660000
 #define ROCKCHIP_CPU_RK3568		0x35680000
 
@@ -153,6 +154,17 @@ static inline bool cpu_is_rk3308(void)
 static inline bool cpu_is_rk3308(void) { return false; }
 #endif
 
+#if defined(CONFIG_CPU_RK3528)
+static inline bool cpu_is_rk3528(void)
+{
+	if (rockchip_soc_id)
+		return (rockchip_soc_id & ROCKCHIP_CPU_MASK) == ROCKCHIP_CPU_RK3528;
+	return of_machine_is_compatible("rockchip,rk3528");
+}
+#else
+static inline bool cpu_is_rk3528(void) { return false; }
+#endif
+
 #if defined(CONFIG_CPU_RK3568)
 static inline bool cpu_is_rk3566(void)
 {
@@ -185,6 +197,7 @@ static inline bool cpu_is_rk3568(void) { return false; }
 #define ROCKCHIP_SOC_RK3288W    (ROCKCHIP_CPU_RK3288 | 0x01)
 #define ROCKCHIP_SOC_RK3308	(ROCKCHIP_CPU_RK3308 | 0x00)
 #define ROCKCHIP_SOC_RK3308B	(ROCKCHIP_CPU_RK3308 | 0x01)
+#define ROCKCHIP_SOC_RK3528	(ROCKCHIP_CPU_RK3528 | 0x00)
 #define ROCKCHIP_SOC_RK3566	(ROCKCHIP_CPU_RK3566 | 0x00)
 #define ROCKCHIP_SOC_RK3568	(ROCKCHIP_CPU_RK3568 | 0x00)
 
@@ -208,6 +221,7 @@ ROCKCHIP_SOC(rk3288, RK3288)
 ROCKCHIP_SOC(rk3288w, RK3288W)
 ROCKCHIP_SOC(rk3308, RK3308)
 ROCKCHIP_SOC(rk3308b, RK3308B)
+ROCKCHIP_SOC(rk3528, RK3528)
 ROCKCHIP_SOC(rk3566, RK3566)
 ROCKCHIP_SOC(rk3568, RK3568)
 
