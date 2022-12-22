c4ffb5aa4cfd4d910af1b718874536bd64de38d9
William Wu <william.wu@rock-chips.com>
2022-12-22 14:59:33 +0800

    mfd: fusb302: Fix no response to get sink cap from source
    The fusb302 use the legacy driver on RK3399 kernel-4.4/4.19 platforms.
    The legacy driver doesn't support to send sink capabilities message,
    it cause RK3399 Type-C USB ADB fail to connected to MacBook with a
    Type-C to Type-C cable.
    
    This patch adds the sink capabilities message and migrate workqueue to
    RT priority for processing pd messages.
    
    1. Add sink capabilities message for fixed supply and battery supply.
    2. Migrate workqueue to RT priority refer to the
       commit 3ed8e1c2ac9914a2fcb08ec13476b85319536cea upstream.
    3. Move the interrupt handling from a threaded interrupt handler to
       a non-threaded interrupt handler which normally queues the new
       interrupt handling work directly.
    
    Signed-off-by: William Wu <william.wu@rock-chips.com>
    Change-Id: I13df67d71d1f0c11f4256daacd2243dcfb8ae414

--- drivers/mfd/fusb302.h
+++ drivers/mfd/fusb302.h
@@ -482,8 +482,8 @@ struct fusb30x_chip {
 	struct i2c_client *client;
 	struct device *dev;
 	struct regmap *regmap;
-	struct work_struct work;
-	struct workqueue_struct *fusb30x_wq;
+	struct kthread_work irq_work;
+	struct kthread_worker *irq_worker;
 	struct hrtimer timer_state_machine;
 	struct hrtimer timer_mux_machine;
 	struct PD_CAP_INFO pd_cap_info;
@@ -525,10 +525,19 @@ struct fusb30x_chip {
 	int msg_id;
 	enum tx_state tx_state;
 	int hardrst_count;
-	u32 source_power_supply[7];
 	/* 50mv unit */
-	u32 source_max_current[7];
+	u32 source_power_supply[7];
 	/* 10ma uint*/
+	u32 source_max_current[7];
+	/* Fixed supply = 0, Battery supply = 1 */
+	u32 sink_supply_type;
+	/* Sink Fixed Supply */
+	u32 sink_volt;
+	u32 sink_opr_cur;
+	/* Sink Battery Supply */
+	u32 sink_max_volt;
+	u32 sink_min_volt;
+	u32 sink_opr_power;
 	int pos_power;
 	/*
 	 * if PartnerCap[0] == 0xffffffff
