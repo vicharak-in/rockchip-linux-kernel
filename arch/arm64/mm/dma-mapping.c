28fd6f3a0421c5d83f756b4d6cd6ec714d39d0ce
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-05-25 11:50:37 +0530

    ARM64: mm: dma-mapping: Increase DMA coherent pool size to 2MB
    RK3399 on android can use more than 256KB/sec from the atomic pool and
    might lead to unwanted behaviours.
    
    This patch might improve the reliability of coherent memory pool
    
    Change-Id: I6cf7f9647003d7c1a81120996d83750b64b13e6f
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- arch/arm64/mm/dma-mapping.c
+++ arch/arm64/mm/dma-mapping.c
@@ -64,7 +64,7 @@ static bool is_dma_coherent(struct device *dev, unsigned long attrs)
 }
 static struct gen_pool *atomic_pool __ro_after_init;
 
-#define DEFAULT_DMA_COHERENT_POOL_SIZE  SZ_256K
+#define DEFAULT_DMA_COHERENT_POOL_SIZE  SZ_2M
 static size_t atomic_pool_size __initdata = DEFAULT_DMA_COHERENT_POOL_SIZE;
 
 static int __init early_coherent_pool(char *p)
